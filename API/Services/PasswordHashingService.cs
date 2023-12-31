2ee95596293161957594a0d61d35b54d26c25d1f
Isaac Pride <thehumbletheorist@gmail.com>
2023-12-31 15:26:19 +0200

    Add PasswordHashingService

---
+++ API/Services/PasswordHashingService.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Services
+{
+    public class PasswordHashingService
+    {
+        private static readonly int _workFactor = 13;
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password, _workFactor);
+        }
+
+        public static bool VerifyPassword(string candidatePassword, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(candidatePassword, passwordHash);
+        }
+    }
+}
