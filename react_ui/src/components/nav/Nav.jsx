208e99f2d4b67ee469498cb2854d49137b6c1954
Isaac Pride <thehumbletheorist@gmail.com>
2024-01-01 15:07:09 +0200

    Update Nav component to conditionally render My Orders and My Profile links based on user login status

--- react_ui/src/components/nav/Nav.jsx
+++ react_ui/src/components/nav/Nav.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import BtnLogInOrOut from '../buttons/BtnLogInOrOut'
 import ControlPanel from '../control-panel/ControlPanel'
-// import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 const Nav = () => {
     const cartIsNotEmpty = useSelector(state => state.carts.loggedInUserCart?.orderLines?.length > 0)
+    const isLoggedIn = useSelector(state => state.currentUser.isLoggedIn)
 
     return (
         <nav className='nav-bar'>
             <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/'>Home</NavLink>
             <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/products'>Products</NavLink>
-            <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/my-orders'>My Orders</NavLink>
-            <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/my-profile'>My Profile</NavLink>
+            {isLoggedIn && <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/my-orders'>My Orders</NavLink>}
+            {isLoggedIn && <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/my-profile'>My Profile</NavLink>}
             <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : ''}`} to='/about'>About</NavLink>
             <NavLink className={({ isActive }) => `nav-bar-link ${isActive ? 'active-nav-link' : (cartIsNotEmpty ? 'cart-has-contents' : '')}`} to='/cart'>Cart</NavLink>
             <ControlPanel />
